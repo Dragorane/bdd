ec77d811e0e906dfea3749cc0ace6bd0c4c4ac53
Loic <lois.laisne@gmail.com>
2014-12-14 23:11:59 +0100

    modification erreur encore acheter/vendre

--- controleurs/acheter.php
+++ controleurs/acheter.php
@@ -22,6 +22,7 @@ class Controller_acheter {
 
     public function acheterbien() {
         $tabcat = categories::list_categ(1);
+        $tabbiens = Controller_acheter::tab_biens();
         include 'vues/acheter/acheterbien.php';
         include "vues/menu_cat.php";
         include "vues/filtre_recherche.php";
@@ -33,6 +34,47 @@ class Controller_acheter {
         include 'vues/acheter/acheterservice.php';
         include "vues/menu_cat.php";
         include "vues/filtre_recherche.php";
+    }
+
+    public static function tab_biens() {
+        if (isset($_GET['idcat'])) {
+            if (isset($_GET['prixcroiss'])) {
+                $tabbien = biens::tabbiens_prix_cat($_GET['idcat']);
+            } else {
+                if (isset($_GET['prixdecroiss'])) {
+                    $tabbien = biens::tabbiens_prixdesc_cat($_GET['idcat']);
+                } else {
+                    if (isset($_GET['evaluation'])) {
+                        $tabbien = biens::tabbiens_eval_cat($_GET['idcat']);
+                    } else {
+                        if (isset($_GET['geoloc'])) {
+                            $tabbien = biens::tabbiens_geoloc_cat($_GET['idcat']);
+                        } else {
+                            $tabbien = biens::tabbiens_cat($_GET['idcat']);
+                        }
+                    }
+                }
+            }
+        } else {
+            if (isset($_GET['prixcroiss'])) {
+                $tabbien = biens::tabbiens_prix();
+            } else {
+                if (isset($_GET['prixdecroiss'])) {
+                    $tabbien = biens::tabbiens_prixdesc();
+                } else {
+                    if (isset($_GET['evaluation'])) {
+                        $tabbien = biens::tabbiens_eval();
+                    } else {
+                        if (isset($_GET['geoloc'])) {
+                            $tabbien = biens::tabbiens_geoloc();
+                        } else {
+                            $tabbien = biens::tabbiens();
+                        }
+                    }
+                }
+            }
+        }
+        return $tabbien;
     }
 
 }
