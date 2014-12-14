4de8a1a52ce9446e19e3dec9857df68ff8c70c0c
Loic <lois.laisne@gmail.com>
2014-12-14 17:13:23 +0100

    brain fuck la suite, temps d'une pause

--- vues/vendre/index.php
+++ vues/vendre/index.php
@@ -1,4 +1,4 @@
 <h2 class='center'>Vendre un Bien/Service</h2>
 
-<h3 class='center'><a href='index.php/vendrebien'>Je veux vendre un Bien</a></h3>
-<h3 class='center'><a href='index.php/vendreservice'>Je veux vendre un Service</a></h3>
+<h3 class='center'><a href='<?= BASEURL ?>/index.php/vendrebien'>Je veux vendre un Bien</a></h3>
+<h3 class='center'><a href='<?= BASEURL ?>/index.php/vendreservice'>Je veux vendre un Service</a></h3>
