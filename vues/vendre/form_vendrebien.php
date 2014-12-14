dd04cac0dec0010038c43105919cfe46f1afbe66
Loic <lois.laisne@gmail.com>
2014-12-14 23:44:19 +0100

    ajout de html au formulaire pour meilleur mise en form

--- vues/vendre/form_vendrebien.php
+++ vues/vendre/form_vendrebien.php
@@ -4,19 +4,20 @@
     <form action="<?= BASEURL ?>/index.php/valid_form_vendre" method="post">
         <input type="hidden" name="idcat" value="<?php echo $_GET['idcat'] ?>">
         <label for="titre">Titre de votre bien</label><br/>
-        <input type="text" name="titre" id="titre"/>
-        <label for="desc">Description de votre bien</label>
-        <textarea rows="5" cols="50" name="desc" id="desc"></textarea>
-        <label for="etat">Etat du bien </label>
+        <input type="text" name="titre" id="titre"/><br/><br/>
+        <label for="desc">Description de votre bien</label><br/>
+        <textarea rows="5" cols="50" name="desc" id="desc"></textarea><br/><br/>
+        <label for="etat">Etat du bien </label><br/>
         <select name="etat">
             <?php
             for ($i = 0; $i < count($tabetat); $i++) {
                 echo "<option value='" . $tabetat[$i]->get_id() . "'>" . $tabetat[$i]->get_lib() . "</option>";
             }
             ?>
-        </select>
-        <label for="prix">Prix (en Pts)</label>
-        <input type="text" name="prix" id="prix"/>
+        </select><br/><br/>
+        <label for="prix">Prix (en Pts)</label><br/>
+        <input type="text" name="prix" id="prix"/><br/><br/>
+        <center><input type="submit" value="Vendre" name="valid_bien"></center>
     </form>
 </div>
 
