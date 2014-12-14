d639a776e00a73a037731dadb3ee84deb8cdd4c4
Loic <lois.laisne@gmail.com>
2014-12-14 15:44:44 +0100

    ajout du format png au control

--- controleurs/utilisateur.php
+++ controleurs/utilisateur.php
@@ -88,7 +88,7 @@ class Controller_Utilisateur {
         // on vérifie maintenant l'extension
         $type_file = $photo['type'];
 
-        if (!strstr($type_file, 'jpg') && !strstr($type_file, 'jpeg') && !strstr($type_file, 'bmp') && !strstr($type_file, 'gif')) {
+        if (!strstr($type_file, 'jpg') && !strstr($type_file, 'jpeg') && !strstr($type_file, 'bmp') && !strstr($type_file, 'gif')&& !strstr($type_file, 'png')) {
             exit("Le fichier n'est pas une image");
         }
         // on copie le fichier dans le dossier de destination
