e57a94e3c4a34e2c01ff92b1037b39776650fea2
Loic <lois.laisne@gmail.com>
2014-12-14 22:14:44 +0100

    petit bug avec bouton fixe

--- vues/filtre_recherche.php
+++ vues/filtre_recherche.php
@@ -1,7 +1,7 @@
 <div class='filtre_recherche'>
     <?php
     if (isset($_GET['idcat'])) {
-        echo "<h3 class='bouton_filtre'><a href='?prixcroiss&idcat=" . $_GET['idcat'] . ">Recherche par prix croissant</a></h3>";
+        echo "<h3 class='bouton_filtre'><a href='?prixcroiss&idcat=" . $_GET['idcat'] . "'>Recherche par prix croissant</a></h3>";
         echo "<h3 class='bouton_filtre'><a href='?prixdecroiss&idcat=" . $_GET['idcat'] . "'>Recherche par prix décroissant</a></h3>";
         echo "<h3 class='bouton_filtre'><a href='?evaluation&idcat=" . $_GET['idcat'] . "'>Recherche par évaluation positive</a></h3>";
         echo "<h3 class='bouton_filtre'><a href='?geoloc&idcat=" . $_GET['idcat'] . "'>Recherche par géolocalisation</a></h3>";
