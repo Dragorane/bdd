e8bab2545485cc782a51cd5801e9c90ef4a95dc8
Loic <lois.laisne@gmail.com>
2014-12-14 17:07:37 +0100

    brain fuck sur les vues

--- vues/acheter/index.php
+++ vues/acheter/index.php
@@ -1,8 +1,4 @@
-<?php
+<h2 class='center'>Acheter un Bien/Service</h2>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<h3 class='center'><a href='index.php/acheterbien'>Je veux acheter un Bien</a></h3>
+<h3 class='center'><a href='index.php/acheterservice'>Je veux acheter un Service</a></h3>
