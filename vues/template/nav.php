27687d71773cf5a6769397f495bb69c197bb1436
Loic <lois.laisne@gmail.com>
2014-12-14 16:57:27 +0100

    maj menu principal

--- vues/template/nav.php
+++ vues/template/nav.php
@@ -1,7 +1,7 @@
 <div id="menu">
     <ul>
         <li><a href="<?= BASEURL ?>/index.php">Page principale</a></li>
-<!--        <li><a href="<?= BASEURL ?>/index.php/objet_form">Ajout d'un objet</a></li>
-        <li><a href="<?= BASEURL ?>/index.php/all_object">Liste des objets</a></li>-->
+        <li><a href="<?= BASEURL ?>/index.php/acheter">Acheter</a></li>
+        <li><a href="<?= BASEURL ?>/index.php/vendre">Vendre</a></li>
     </ul>
 </div>
