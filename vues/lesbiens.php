99c3074e83ae556e9580a2f9050fdae5affd5b9d
Loic <lois.laisne@gmail.com>
2014-12-17 13:49:27 +0100

    retrait des lignes de debugg, suppression bug affichage dans achat d'un bien

--- vues/lesbiens.php
+++ vues/lesbiens.php
@@ -2,13 +2,15 @@
 
 echo "<div class='lesbiens'>";
 for ($i = 0; $i < count($tabbiens); $i++) {
-    $uti = Utilisateur::get_by_id($tabbiens[i]->get_uti());
-    echo "<div class='unbien'>";
-    echo "<h3>" . $tabbiens[$i]->get_titre() . "</h3>";
-    echo "<p>" . wordwrap($tabbiens[i]->get_desc(), 50) . "</p>";
-    echo "<p> Vendu au prix de : " . $tabbiens[i]->get_prix() . " Pts par l'utilisateur <a href='index.php/pageuti?pseudo='" . $uti->pseudo() . "'>'" . $uti->pseudo() . "'</a> </p>";
-    echo "<h4 class='center'><a href='pagebien?id=" . $tabbiens[$i]->get_id() . "'>Acheter / En savoir plus ...</h4>";
-    echo "</div>";
+    $uti = Utilisateur::get_by_id($tabbiens[$i]->get_uti());
+    if ($uti != NULL) {
+        echo "<div class='unbien'>";
+        echo "<h3>" . $tabbiens[$i]->get_titre() . "</h3>";
+        echo "<p>" . wordwrap($tabbiens[$i]->get_desc(), 50) . "</p>";
+        echo "<p> Vendu au prix de : " . $tabbiens[$i]->get_prix() . " Pts par l'utilisateur <a href='index.php/pageuti?pseudo='" . $uti->pseudo() . "'>'" . $uti->pseudo() . "'</a> </p>";
+        echo "<h4 class='center'><a href='pagebien?id=" . $tabbiens[$i]->get_id() . "'>Acheter / En savoir plus ...</h4>";
+        echo "</div>";
+    }
 }
 echo "</div>";
 ?>
