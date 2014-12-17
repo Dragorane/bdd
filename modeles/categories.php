a706aa6b64b3b72d26a3611dd3c64cc3e0fcb979
Loic <lois.laisne@gmail.com>
2014-12-17 13:06:31 +0100

    correction bug ctegorie-bien

--- modeles/categories.php
+++ modeles/categories.php
@@ -50,7 +50,7 @@ class categories extends Model_Base {
 
     public static function recupCat($id) {
         $cat = null;
-        echo "l'id de la cat : ".$_GET['id'];
+        echo "l'id de la cat : " . $id;
         if (is_integer($id)) {
             $query = "select * from Categorie where idCat=:id";
             $stmt = @oci_parse(Model_Base::$_db, $query) or die("erreur insertion categrie" . oci_error($conn));
