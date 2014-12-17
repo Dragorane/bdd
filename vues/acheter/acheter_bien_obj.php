4fc82a81447b0fdfaa459a920fa8ba893c1f2955
Loic <lois.laisne@gmail.com>
2014-12-18 00:49:34 +0100

    debugg affichage

--- vues/acheter/acheter_bien_obj.php
+++ vues/acheter/acheter_bien_obj.php
@@ -4,21 +4,21 @@
 
 <form method='POST' name='form_acheter_bien_pts' action='<?= BASEURL ?>/index.php/valid_acheter_bien_obj'>
     <input type='hidden' name='bien' value='<?php echo $bien->get_id(); ?>'>
-    <label for='obj'>Bien ou service à échanger</label>
+    <label for='obj'>Bien ou service à échanger</label><br/>
     <select name='obj'>
         <?php
         echo "  <optgroup label='Vos biens'>";
         for ($i = 0; $i < count($tabbien); $i++) {
-            echo "<option value='bien:" . $tabbien[$i]->get_id() . "'></option>";
+            echo "<option value='bien:" . $tabbien[$i]->get_id() . "'>" . $tabbien[$i]->get_titre() . "</option>";
         }
         echo "</optgroup>";
         echo "  <optgroup label='Vos biens'>";
         for ($j = 0; $j < count($tabserv); $j++) {
-            echo "<option value='serv:" . $tabserv[$j]->get_id() . "'></option>";
+            echo "<option value='serv:" . $tabserv[$j]->get_id() . "'>" . $tabserv[$i]->get_titre() . "</option>";
         }
         echo "</optgroup>";
         ?>
-    </select>
+    </select><br/><br/>
     <label for='adr'>Adresse de l'échange</label><br/>
     <input type='text' name='adr' id='adr'/><br/><br/>
     <label for='date'>Date de l'échange (format dd/mm/aaaa)</label><br/>
