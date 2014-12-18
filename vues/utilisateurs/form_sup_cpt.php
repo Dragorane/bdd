de752c41a79d88b05048eeeffa251d926b695ef5
SIben <chetioui.benjamin@gmail.com>
2014-12-18 02:48:18 +0100

    Jabadabadou

--- vues/utilisateurs/form_sup_cpt.php
+++ vues/utilisateurs/form_sup_cpt.php
@@ -3,7 +3,7 @@
 <div class='div_form_sup_cpt'>
 		<form action="<?= BASEURL ?>/index.php/valid_form_sup_cpt" method="post">
 		<input type="hidden" value="<?php echo $_SESSION['pseudo'];?>">
-        <input type="submit" value="Oui">
-        <input type="submit" value="Non">
+        <input type="submit" name="Oui" value="Oui">
+        <input type="submit" name="Non" value="Non">
     </form>
 </div>
