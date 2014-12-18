4ac5dfd91f00d7eea672734992e111384a7d0a82
Loic <lois.laisne@gmail.com>
2014-12-18 05:37:39 +0100

    design

--- vues/vendre/form_vendrebien.php
+++ vues/vendre/form_vendrebien.php
@@ -1,24 +1,25 @@
 <h2 class='center'>Mise en vente d'un Bien</h2>
-<h3>Vous avez sélectionnez la catégorie : <?php echo $cat->getlib(); ?></h3>
+<div class="div_separer">
+    <h3>Vous avez sélectionnez la catégorie : <?php echo $cat->getlib(); ?></h3>
 
-<div class="form_vendre">
-    <form action="<?= BASEURL ?>/index.php/valid_form_vendre_bien" method="post">
-        <input type="hidden" name="idcat" value="<?php echo $_GET['idcat'] ?>">
-        <label for="titre">Titre de votre bien</label><br/>
-        <input type="text" name="titre" id="titre"/><br/><br/>
-        <label for="desc">Description de votre bien</label><br/>
-        <textarea rows="5" cols="50" name="desc" id="desc"></textarea><br/><br/>
-        <label for="etat">Etat du bien </label><br/>
-        <select name="etat">
-            <?php
-            for ($i = 0; $i < count($tabetat); $i++) {
-                echo "<option value='" . $tabetat[$i]->get_id() . "'>" . $tabetat[$i]->get_lib() . "</option>";
-            }
-            ?>
-        </select><br/><br/>
-        <label for="prix">Prix (en Pts)</label><br/>
-        <input type="text" name="prix" id="prix"/><br/><br/>
-        <center><input type="submit" value="Vendre" name="valid_bien"></center>
-    </form>
+    <div class="form_vendre">
+        <form action="<?= BASEURL ?>/index.php/valid_form_vendre_bien" method="post">
+            <input type="hidden" name="idcat" value="<?php echo $_GET['idcat'] ?>">
+            <label for="titre">Titre de votre bien</label><br/>
+            <input type="text" name="titre" id="titre"/><br/><br/>
+            <label for="desc">Description de votre bien</label><br/>
+            <textarea rows="5" cols="50" name="desc" id="desc"></textarea><br/><br/>
+            <label for="etat">Etat du bien </label><br/>
+            <select name="etat">
+                <?php
+                for ($i = 0; $i < count($tabetat); $i++) {
+                    echo "<option value='" . $tabetat[$i]->get_id() . "'>" . $tabetat[$i]->get_lib() . "</option>";
+                }
+                ?>
+            </select><br/><br/>
+            <label for="prix">Prix (en Pts)</label><br/>
+            <input type="text" name="prix" id="prix"/><br/><br/>
+            <center><input type="submit" value="Vendre" name="valid_bien"></center>
+        </form>
+    </div>
 </div>
-
