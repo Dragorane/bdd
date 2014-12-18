8288a4d655785855df4d8a615bbfad5d8d79f809
Loic <lois.laisne@gmail.com>
2014-12-18 05:15:26 +0100

    design

--- vues/afficher_bien.php
+++ vues/afficher_bien.php
@@ -3,5 +3,4 @@
 echo "<h3>" . $bien->get_titre() . "</h3>";
 echo "<p>" . $bien->get_desc() . "</p>";
 echo "<p> Vendu au prix de : " . $bien->get_prix() . " Pts par l'utilisateur <a href='" . BASEURL . "/index.php/pageuti?pseudo=" . $uti->pseudo() . "'>'" . $uti->pseudo() . "'</a> </p>";
-
 ?>
