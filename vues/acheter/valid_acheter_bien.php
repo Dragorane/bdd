545858ee2f434083c922448245e5d20702a486e4
Loic <lois.laisne@gmail.com>
2014-12-18 06:01:28 +0100

    design

--- vues/acheter/valid_acheter_bien.php
+++ vues/acheter/valid_acheter_bien.php
@@ -1,7 +1,9 @@
 <h2 class='center'>Acheter un bien</h2>
 
 <div class='success'><p>Votre achat a été validé. </p></div>
-<p> Le propriétaire de l'objet a reçu votre proposition.
-    <br/> S'il valide votre proposition, il prendra contact avec vous par mail afin de procéder à l'échange.
-    <br/> Merci pour votre confiance.
-</p>
+<div class='div_page_bien_serv'>
+    <p> Le propriétaire de l'objet a reçu votre proposition.
+        <br/> S'il valide votre proposition, il prendra contact avec vous par mail afin de procéder à l'échange.
+        <br/> Merci pour votre confiance.
+    </p>
+</div>
--- vues/acheter/valid_acheter_serv.php
+++ vues/acheter/valid_acheter_serv.php
@@ -1,7 +1,9 @@
 <h2 class='center'>Acheter un service avec vos Points trocs</h2>
 
 <div class='success'><p>Votre achat a été validé. </p></div>
-<p> Le propriétaire du service a reçu votre proposition.
-    <br/> S'il valide votre proposition, il prendra contact avec vous par mail afin de procéder à l'échange.
-    <br/> Merci pour votre confiance.
-</p>
+<div class='div_page_bien_serv'>
+    <p> Le propriétaire du service a reçu votre proposition.
+        <br/> S'il valide votre proposition, il prendra contact avec vous par mail afin de procéder à l'échange.
+        <br/> Merci pour votre confiance.
+    </p>
+</div>
