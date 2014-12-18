b3ae7bf13c41d68d76086a80b583fec74db8633f
Loic <lois.laisne@gmail.com>
2014-12-18 04:12:28 +0100

    design

--- vues/afficher_service.php
+++ vues/afficher_service.php
@@ -1,5 +1,4 @@
 <?php
-
 echo "<h3>" . $serv->get_titre() . "</h3>";
 echo "<p>" . $serv->get_desc() . "</p>";
 echo "<p> Vendu au prix de : " . $serv->get_prix() . " Pts par l'utilisateur <a href='" . BASEURL . "/index.php/pageuti?pseudo=" . $uti->pseudo() . "'>'" . $uti->pseudo() . "'</a> </p>";
