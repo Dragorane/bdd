2c045e5d8595a610c1986e807f4438b265d0fc7b
Loic <lois.laisne@gmail.com>
2014-12-18 06:07:53 +0100

    design

--- vues/proposition/propositions.php
+++ vues/proposition/propositions.php
@@ -2,7 +2,7 @@
 
 <h3 class='center'> Proposition où vous vendez</h3>
 <table class='tab_visible'>
-    <tr><th>Acheteur</th><th>prix</th><th>date</th><th>lieux</th></tr>
+    <tr><th>Acheteur</th><th>prix</th><th>date</th><th>lieu</th></tr>
     <?php
     for ($i = 0; $i < count($prop_vendeur); $i++) {
         $uti_acheteur = Utilisateur::get_by_id($prop_vendeur[$i]->get_iduti());
@@ -11,6 +11,7 @@
             echo "<td>" . $uti_acheteur->pseudo() . "</td>";
             echo "<td>" . $prop_vendeur[$i]->get_prix() . "</td>";
             echo "<td>" . $prop_vendeur[$i]->get_date() . "</td>";
+            echo "<td>" . $prop_vendeur[$i]->get_adr() . "</td>";
             echo "<td>";
             echo "<form name='voir_prop' method='POST' action='" . BASEURL . "/index.php/laproposition'>"
             . "<input type='hidden' name='laprop' value='" . $prop_vendeur[$i]->get_id() . "'/>"
@@ -26,7 +27,7 @@
 
 <h3 class='center'>Proposition où vous achetez</h3>
 <table class='tab_visible'>
-    <tr><th>Vendeur</th><th>prix</th><th>date</th></tr>
+    <tr><th>Vendeur</th><th>prix</th><th>date</th><th>lieu</th></tr>
     <?php
     for ($i = 0; $i < count($prop_vendeur); $i++) {
         $uti_vendeur = Utilisateur::get_by_id($prop_vendeur[$i]->get_idv());
@@ -34,6 +35,7 @@
         echo "<td>" . $uti_vendeur->pseudo() . "</td>";
         echo "<td>" . $prop_vendeur[$i]->get_prix() . "</td>";
         echo "<td>" . $prop_vendeur[$i]->get_date() . "</td>";
+        echo "<td>" . $prop_vendeur[$i]->get_adr() . "</td>";
         echo "<td>";
         echo "<form name='voir_prop' method='POST' action='" . BASEURL . "/index.php/laproposition'>"
         . "<input type='hidden' name='laprop' value='" . $prop_vendeur[$i]->get_id() . "'/>"
