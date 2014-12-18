20646393f866cf7df53dec770e3667cc39f96579
Loic <lois.laisne@gmail.com>
2014-12-18 06:08:47 +0100

    design

--- vues/proposition/proposition_vendeur.php
+++ vues/proposition/proposition_vendeur.php
@@ -7,12 +7,12 @@
     echo "<p>Acheteur : " . $utiacheteur->pseudo() . "</p>";
     echo "<p>Prix en points trocs de la proposition : " . $prop->get_prix() . "</p>";
     echo "<p>Date de la proposition : " . $prop->get_date() . "</p>";
-    ?>
+    ?><br/><br/>
     <center>
         <form method='POST' action='<?= BASEURL ?>/index.php/valider_prop'>
             <input type='hidden' name='idprop' value='<?php echo $prop->get_id(); ?>'>
             <input type='submit' name='valid_prop' value='Accepter la proposition' class='submit'>
-        </form><br/><br/>
+        </form><br/>
         <form method='POST' action='<?= BASEURL ?>/index.php/annuler_prop'>
             <input type='hidden' name='idprop' value='<?php echo $prop->get_id(); ?>'>
             <input type='submit' name='sup_prop' value='Refuser/Annuler la proposition' class='submit'>
